70a7b992f481a163cfcec38dbcd080af44f2e7df
littleclown <xiaozhekudexc@hotmail.com>
2018-01-16 13:38:46 +0800

    add utils.

---
+++ src/utils/index.ts
@@ -0,0 +1,18 @@
+import * as lodash from 'lodash'
+import { INFO, EmmLogger } from 'emm-logger'
+import * as localUtils from '@utils'
+
+
+export const logger = new EmmLogger('ws-ts', {
+  level: INFO,
+})
+
+
+// `as any` can skip typescript's type checking.
+export default new Proxy({}, {
+  get: function(target, property, receiver) {
+    if( property in localUtils ) return localUtils[property]
+    if( property in lodash ) return lodash[property]
+    throw new ReferenceError(`Property "${property}" does not exist.`)
+  }
+}) as any
